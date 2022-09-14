5e23f40b74388aba1d73a42f32fe077b0bbc2d69
mislavskii <jornjat@gmail.com>
2022-09-14 23:42:45 +0700

    getter setter

--- src/main/java/org/example/Cat.java
+++ src/main/java/org/example/Cat.java
@@ -1,14 +1,22 @@
 package org.example;
 
 public class Cat {
-    public String name = "Мурзик";  // свойство общее для класса
+    private String name = "Мурзик";  // свойство общее для класса
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public void printName() {  // метод
         System.out.println("Имя котика: " + name);
     }
 
     public static void sayMeow(int n) {
-        for (int i=0; i <= n; i++) {
+        for (int i=0; i<n; i++) {
             System.out.println("Мяу !!");
         }
     }
