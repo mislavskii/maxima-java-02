391c5fcdd6d4001c045519d9ab04a3af8e1b8044
mislavskii <jornjat@gmail.com>
2022-09-24 07:06:03 +0300

    improved factory

--- src/main/java/org/example/TransportFactory.java
+++ src/main/java/org/example/TransportFactory.java
@@ -1,34 +1,24 @@
 package org.example;
 
 public class TransportFactory {
-    private String TRUCK_NAME = "Globetrotter";
-    private float TRUCK_COST_OF_KM = 23;
-    private String SHIP_NAME = "Maelstrom";
-    private float SHIP_COST_OF_KM = 420;
-    private String PLANE_NAME = "Thunderbolt";
-    private float PLANE_COST_OF_KM = 5400;
+    private static String TRUCK_NAME = "Globetrotter";
+    private static float TRUCK_COST_OF_KM = 23;
+    private static String SHIP_NAME = "Maelstrom";
+    private static float SHIP_COST_OF_KM = 420;
+    private static String PLANE_NAME = "Thunderbolt";
+    private static float PLANE_COST_OF_KM = 5400;
 
-    public Transport getTransport(City city, int weight, int hours) {
+    public static Transport getTransport(City city, int weight, int hours) {
         float requiredSpeed = (float) city.getDistanceKm() / hours;
-        int transportSpeed;
-        if (requiredSpeed % 10 != 0) {
-            transportSpeed = (int) requiredSpeed / 10 * 10 + 10;
-        } else {
-            transportSpeed = (int) requiredSpeed;
-        }
-        int transportCapacity;
-        if (weight % 500 != 0) {
-            transportCapacity = weight / 500 * 500 + 500;
-        } else {
-            transportCapacity = weight;
-        }
+        int transportSpeed = requiredSpeed % 10 == 0 ? (int) requiredSpeed : (int) requiredSpeed / 10 * 10 + 10;
+        int transportCapacity = weight % 500 == 0 ? weight : weight / 500 * 500 + 500;
         if (requiredSpeed < 40 && city.isOnWater()) {
-            return new Ship (SHIP_NAME, transportCapacity, transportSpeed, SHIP_COST_OF_KM);
-        } else if (requiredSpeed > 120 && city.hasAirport()) {
-            return new Plane(PLANE_NAME, transportCapacity, transportSpeed, PLANE_COST_OF_KM);
-        } else {
-            return new Truck(TRUCK_NAME, transportCapacity, transportSpeed, TRUCK_COST_OF_KM);
+            return new Ship(SHIP_NAME, transportCapacity, transportSpeed, SHIP_COST_OF_KM);
         }
+        if (requiredSpeed > 120 && city.hasAirport()) {
+            return new Plane(PLANE_NAME, transportCapacity, transportSpeed, PLANE_COST_OF_KM);
+        }
+        return new Truck(TRUCK_NAME, transportCapacity, transportSpeed, TRUCK_COST_OF_KM);
     }
 
 }
